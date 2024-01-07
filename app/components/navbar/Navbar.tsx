25be3921d484e695f93015c1bb484bf4f8ca4669
7FyD <80410162+7FyD@users.noreply.github.com>
2024-01-07 03:38:15 +0200

    Initial commit.

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <div className="relative max-w-[250px]">
+      <p className="text-3xl cursor-pointer">&#9776; open</p>
+    </div>
+  );
+};
+
+export default Navbar;
