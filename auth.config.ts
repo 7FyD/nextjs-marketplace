281d5266b869e61f1792d6c14faa733d33c9aa4e
7FyD <80410162+7FyD@users.noreply.github.com>
2024-01-20 13:39:10 +0200

    chore: improve whitespace

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,13 @@
-import bcrypt from "bcryptjs";
-import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
+import type { NextAuthConfig } from "next-auth";
+
+import bcrypt from "bcryptjs";
+
 import { LoginSchema } from "@/schemas";
-import { getUserByEmail } from "@/data/user";
+import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
