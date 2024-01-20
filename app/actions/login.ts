cc178f50d4a53bb35e0bf8e9459d6330b0a43213
7FyD <80410162+7FyD@users.noreply.github.com>
2024-01-20 16:25:27 +0200

    chore: improve login failed message

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -6,7 +6,7 @@ import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 import { getUserByEmail } from "@/data/user";
-import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -16,6 +16,12 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email or password invalid." };
+  }
 
   try {
     await signIn("credentials", {
@@ -28,7 +34,7 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
-          return { error: "Invalid credentials!" };
+          return { error: "Email or password invalid." };
         default: {
           return { error: "Something went wrong!" };
         }
