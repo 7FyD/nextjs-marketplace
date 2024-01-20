c30c84927020abd484a054f6a7629100201f3485
7FyD <80410162+7FyD@users.noreply.github.com>
2024-01-20 13:38:28 +0200

    feat: add OAuth login for google and github

--- app/components/auth/social.tsx
+++ app/components/auth/social.tsx
@@ -6,16 +6,17 @@ import { FaGithub } from "react-icons/fa";
 import { useSearchParams } from "next/navigation";
 
 import { Button } from "@/app/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
-  // const onClick = (provider: "google" | "github") => {
-  //   signIn(provider, {
-  //     callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
-  //   });
-  // };
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+    });
+  };
 
   return (
     <div className="flex items-center w-full gap-x-2">
@@ -23,7 +24,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => console.log("Google clicked!")}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -31,7 +32,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => console.log("Github clicked!")}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
