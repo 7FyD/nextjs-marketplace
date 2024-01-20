0954d7d7af519a7c893ad4253c969add65a4f800
7FyD <80410162+7FyD@users.noreply.github.com>
2024-01-20 13:30:55 +0200

    feat: add USER/ADMIN role for each user, implement auth session/jwt callbacks

--- auth.ts
+++ auth.ts
@@ -1,8 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "./data/user";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -13,6 +21,40 @@ export const {
   pages: {
     signIn: "/auth/login",
     error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role === "USER" ? "USER" : "ADMIN";
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
