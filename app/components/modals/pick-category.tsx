79510c9fdee8b83579a02a911dae85799c9111c1
7FyD <80410162+7FyD@users.noreply.github.com>
2024-02-23 21:54:51 +0200

    feat: improve filter listings component

--- app/components/modals/pick-category.tsx
+++ app/components/modals/pick-category.tsx
@@ -14,10 +14,14 @@ import PickCategoryButton from "./pick-category-button";
 
 interface PickCategoryModalProps {
   onSelectCategory: (category: string) => void;
+  className?: string;
+  key?: number;
 }
 
 const PickCategoryModal: React.FC<PickCategoryModalProps> = ({
   onSelectCategory,
+  className,
+  key,
 }) => {
   const [selectedCategory, setSelectedCategory] = useState("");
 
@@ -27,15 +31,17 @@ const PickCategoryModal: React.FC<PickCategoryModalProps> = ({
   };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild className="block">
+    <Dialog key={key}>
+      <DialogTrigger asChild className={`block ${className}`}>
         <Button variant="outline">
           {selectedCategory === ""
             ? "Select a category"
-            : `${selectedCategory} - Change`}
+            : `${selectedCategory} - change`}
         </Button>
       </DialogTrigger>
-      <DialogContent className="overflow-y-scroll max-h-[80%] sm:max-w-2xl p-16 bg-white text-black">
+      <DialogContent
+        className={`overflow-y-scroll max-h-[80%] sm:max-w-2xl p-16 bg-white text-black`}
+      >
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Pick your item's category
@@ -53,22 +59,22 @@ const PickCategoryModal: React.FC<PickCategoryModalProps> = ({
           <PickCategoryButton
             label="Smartphones"
             handleClick={handleCategorySelect}
-          />{" "}
+          />
           <PickCategoryButton
             label="PC Hardware"
             handleClick={handleCategorySelect}
-          />{" "}
+          />
           <PickCategoryButton
             label="PC Periferics"
             handleClick={handleCategorySelect}
-          />{" "}
-          <PickCategoryButton label="Etc" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="vad" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="eu" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="ce" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="mai" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="pun" handleClick={handleCategorySelect} />{" "}
-          <PickCategoryButton label="aici" handleClick={handleCategorySelect} />{" "}
+          />
+          <PickCategoryButton label="Etc" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="vad" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="eu" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="ce" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="mai" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="pun" handleClick={handleCategorySelect} />
+          <PickCategoryButton label="aici" handleClick={handleCategorySelect} />
           <PickCategoryButton label="xd" handleClick={handleCategorySelect} />
         </div>
       </DialogContent>
