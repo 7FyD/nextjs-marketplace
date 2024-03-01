637d7083f61c640e197fea233530efba40eabfa9
7FyD <80410162+7FyD@users.noreply.github.com>
2024-03-02 00:59:50 +0200

    style: improve basic user profle look

--- app/(protected)/user/profile/[userId]/UserProfileClient.tsx
+++ app/(protected)/user/profile/[userId]/UserProfileClient.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import ListingsArray from "@/app/components/listings/listings-array";
+import ListingsDisplay from "@/app/components/listings/listings-display";
+import { Button } from "@/app/components/ui/button";
 import Container from "@/app/components/utilities/Container";
+import { useCurrentUser } from "@/app/hooks/use-current-user";
 import { Listing } from "@prisma/client";
+import { Ban, MessageSquareWarning, UserPlus } from "lucide-react";
 import Image from "next/image";
 
 interface UserProfileClientInterface {
@@ -10,32 +13,62 @@ interface UserProfileClientInterface {
     id: string;
     name: string | null;
     image: string | null;
+    email: string | null;
     role: "USER" | "ADMIN";
   };
   userListings: Listing[];
+  totalListingsCount: number;
+  listingsPerPage: number;
 }
 
 const UserProfileClient: React.FC<UserProfileClientInterface> = ({
   user,
   userListings,
+  totalListingsCount,
+  listingsPerPage,
 }) => {
+  const currentUser = useCurrentUser();
   return (
     <Container>
-      <div className="flex flex-col justify-center items-center gap-4 mt-16">
+      <div className="flex flex-col md:flex-row gap-10 md:gap-0 items-center mt-16 p-6 border-2 rounded-full bg-gray-200 ">
         <Image
+          className="rounded-full"
           src={user.image ? user.image : "/images/default.png"}
           width={256}
           height={256}
-          alt="User image"
+          alt={`${user.name} image`}
         />
-        <h2>{user.name}</h2>
+        <div className="flex flex-col justify-between items-center gap-2 mx-auto">
+          <h1 className="font-semibold text-xl">{user.name}</h1>
+          <p>Email: {user.email}</p>
+          <p>Followers: 0</p>
+          <p>Role: {user.role}</p>
+          {user.id !== currentUser?.id && (
+            <div className="flex flex-row gap-4 mt-8">
+              <Button className="bg-blue-500 hover:bg-blue-700 mb-2">
+                Follow <UserPlus className="ml-3" />
+              </Button>
+              <Button className="bg-red-500 hover:bg-red-700">
+                Report <MessageSquareWarning className="ml-3" />
+              </Button>
+              {user.role !== "ADMIN" && currentUser?.role === "ADMIN" && (
+                <Button className="bg-red-800 hover:bg-red-950">
+                  Ban user <Ban className="ml-3" />
+                </Button>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      <p>User listings: </p>
-      {userListings.length > 0 ? (
-        <ListingsArray listings={userListings} />
-      ) : (
-        "Nothing found."
-      )}
+      <h1 className="text-center font-semibold text-xl mt-6">
+        User listings:{" "}
+      </h1>
+      <ListingsDisplay
+        listings={userListings}
+        totalListingsCount={totalListingsCount}
+        listingsPerPage={listingsPerPage}
+        defaultHidden={true}
+      />
     </Container>
   );
 };
--- app/(protected)/user/profile/[userId]/page.tsx
+++ app/(protected)/user/profile/[userId]/page.tsx
@@ -11,14 +11,24 @@ const UserProfilePage = async ({ params }: { params: IParams }) => {
     return <p>Invalid search.</p>;
   const user = await getUserById(params.userId);
   if (!user) return <p>Not found!</p>;
-  const { listings } = await getListings({ userId: user.id });
+  const { listings, totalListingsCount, listingsPerPage } = await getListings({
+    userId: user.id,
+  });
   const safeUser = {
     id: user.id,
     name: user.name,
     image: user.image,
+    email: user.email,
     role: user.role,
   };
-  return <UserProfileClient user={safeUser} userListings={listings} />;
+  return (
+    <UserProfileClient
+      user={safeUser}
+      userListings={listings}
+      totalListingsCount={totalListingsCount}
+      listingsPerPage={listingsPerPage}
+    />
+  );
 };
 
 export default UserProfilePage;
