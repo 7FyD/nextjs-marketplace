edfaae421684793ceaf520c2d9d3e7eefa6c0cd6
7FyD <80410162+7FyD@users.noreply.github.com>
2024-03-19 23:37:15 +0200

    feat: add security settings schema

--- schemas/settings-schemas.ts
+++ schemas/settings-schemas.ts
@@ -3,12 +3,29 @@ import { phoneRegex } from "./listing-schemas";
 
 export const generalSettingsSchema = z.object({
   image: z.optional(z.string()),
-  name: z.optional(z.string()),
-  username: z.optional(z.string()),
+  name: z
+    .optional(
+      z.string().min(2, "Your new name must contain at least 2 characters.")
+    )
+    .or(z.literal("")),
+  username: z
+    .optional(
+      z.string().min(2, "Your new username must contain at least 2 characters.")
+    )
+    .or(z.literal("")),
   phoneNumber: z
     .optional(z.string().regex(phoneRegex, "Invalid phone number!"))
     .or(z.literal("")),
   publicEmail: z.optional(z.string().email()).or(z.literal("")),
   dateOfBirth: z.optional(z.coerce.date()),
   allowFollow: z.optional(z.boolean()),
+});
+
+export const securitySettingsSchema = z.object({
+  email: z.optional(z.string().email()).or(z.literal("")),
+  password: z
+    .optional(
+      z.string().min(6, "Your new password must contain at least 6 characters.")
+    )
+    .or(z.literal("")),
 });
