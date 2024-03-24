be039924f2dab9ea8eff7c597ddd0ac5e080bd7b
7FyD <80410162+7FyD@users.noreply.github.com>
2024-03-25 00:53:48 +0200

    chore: remove punctuation marks in form error messages

--- schemas/listing-schemas.ts
+++ schemas/listing-schemas.ts
@@ -10,18 +10,18 @@ export const NewListingSchema = z.object({
   title: z
     .string()
     .min(2, {
-      message: "A title of minimum 2 characters is required.",
+      message: "A title of minimum 2 characters is required",
     })
     .max(32, {
-      message: "A maximum of 32 characters are allowed for the title.",
+      message: "A maximum of 32 characters are allowed for the title",
     }),
   description: z.string().min(16, {
-    message: "A description of minimum 16 characters is required.",
+    message: "A description of minimum 16 characters is required",
   }),
   price: z.coerce
     .number()
     .nonnegative({
-      message: "Please input a price >= $0.",
+      message: "Please input a price >= $0",
     })
     .finite()
     .refine(
@@ -29,24 +29,24 @@ export const NewListingSchema = z.object({
         if (n % 1 === 0) return true;
         else return n.toString().split(".")[1].length <= 2;
       },
-      { message: "Maximum 2 decimals allowed." }
+      { message: "Maximum 2 decimals allowed" }
     ),
   imageUrl: z.string().min(1, {
     message: "Image is required.",
   }),
   category: z.string().min(1, {
-    message: "Please select your item's category.",
+    message: "Please select your item's category",
   }),
   condition: z.nativeEnum(Condition, {
     errorMap: (issue, ctx) => {
-      return { message: "Please select your item's condition." };
+      return { message: "Please select your item's condition" };
     },
   }),
   country: z.nativeEnum(CountryEnum, {
     errorMap: (issue, ctx) => {
-      return { message: "Please select a country." };
+      return { message: "Please select a country" };
     },
   }),
-  phone: z.string().regex(phoneRegex, "Invalid phone number!"),
+  phone: z.string().regex(phoneRegex, "Invalid phone number"),
   email: z.string().email(),
 });
--- schemas/settings-schemas.ts
+++ schemas/settings-schemas.ts
@@ -5,16 +5,16 @@ export const generalSettingsSchema = z.object({
   image: z.optional(z.string()),
   name: z
     .optional(
-      z.string().min(2, "Your new name must contain at least 2 characters.")
+      z.string().min(2, "Your new name must contain at least 2 characters")
     )
     .or(z.literal("")),
   username: z
     .optional(
-      z.string().min(2, "Your new username must contain at least 2 characters.")
+      z.string().min(2, "Your new username must contain at least 2 characters")
     )
     .or(z.literal("")),
   phoneNumber: z
-    .optional(z.string().regex(phoneRegex, "Invalid phone number!"))
+    .optional(z.string().regex(phoneRegex, "Invalid phone number"))
     .or(z.literal("")),
   publicEmail: z.optional(z.string().email()).or(z.literal("")),
   dateOfBirth: z.optional(z.coerce.date()),
@@ -25,12 +25,12 @@ export const securitySettingsSchema = z.object({
   email: z.optional(z.string().email()).or(z.literal("")),
   password: z
     .optional(
-      z.string().min(6, "Your new password must contain at least 6 characters.")
+      z.string().min(6, "Your new password must contain at least 6 characters")
     )
     .or(z.literal("")),
   code: z.optional(z.string()),
 });
 
 export const deleteAccountSettingsSchema = z.object({
-  code: z.optional(z.string().min(6, "Invalid code!")).or(z.literal("")),
+  code: z.optional(z.string().min(6, "Invalid code")).or(z.literal("")),
 });
